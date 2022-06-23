b27783008b5d37b73f62d15177c0e3f3ced66a59
e.adeli <adeli@EADELI>
2022-06-23 10:29:14 +0430

    add mockservice into project

--- src/app/modules/z-core/core.module.ts
+++ src/app/modules/z-core/core.module.ts
@@ -1,10 +1,18 @@
 import { ErrorHandler, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { SidebarComponent } from './component/sidebar/sidebar.component';
-import { NavComponent } from './component/nav/nav.component';
-import { GlobalErrorHandler } from './handler/global-error-handler';
+import { environment } from 'src/environments/environment';
+import { CoreRoutingModule } from './core-routing.module';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { SharedModule } from '../y-shared/shared.module';
+
+import { GlobalErrorHandler } from './handler/global-error-handler';
+import { InMemoryDataService } from './mocks/in-memory-data-service';
+
+import { NavComponent } from './component/nav/nav.component';
+import { SidebarComponent } from './component/sidebar/sidebar.component';
+
+
 
 @NgModule({
   declarations: [
@@ -13,6 +21,8 @@ import { SharedModule } from '../y-shared/shared.module';
   ],
   imports: [
     CommonModule,
+    CoreRoutingModule,
+    environment.production ? HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { delay: 100 }) : [],
     
     SharedModule
   ],
